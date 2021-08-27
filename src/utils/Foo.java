5f1816ed93e6766d2b07d77a21a6a9e418bf218c
Endermondo Louissaint <elow300@gmail.com>
2021-08-27 06:23:19 -0400

    Print statement from person 2 added

---
+++ src/utils/Foo.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'enday1' branch, from Eclipse");
+		System.out.println("hello worlds");
+		System.out.println("person 2 says hi to everyone");
+	}
+
+}
